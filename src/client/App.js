14723962d333dec7da653c9449699806835f5cd5
pclundaahl <pc.lundaahl@gmail.com>
2018-11-16 21:39:31 -0800

    Import cognito config details into React

--- src/client/App.js
+++ src/client/App.js
@@ -4,17 +4,29 @@ import NavBar from './components/navbar';
 import PublicProfile from './components/publicProfile';
 import axios from 'axios';
 
+import cognitoConfig from './config/cognitoConfig';
+import { CognitoUserPool } from 'amazon-cognito-identity-js';
+
+const userPool = new CognitoUserPool(cognitoConfig);
+
 export default class App extends Component {
   state = {
-    profile: {}
+    profile: {},
+    currentUserToken: {}
   }
 
   // Use this for when a user is first logged in - Phase 2
-  // componentDidMount() {
-  //   fetch('/api/getProfile:{currentUserId}')
-  //     .then(res => res.json())
-  //     .then(user => this.setState({ profile }));
-  // }
+  componentDidMount() {
+    try {
+      this.getCurrentCognitoUser();
+    } catch (err) {
+      console.log(err);
+    }
+
+    // fetch('/api/getProfile:{currentUserId}')
+    //   .then(res => res.json())
+    //   .then(user => this.setState({ profile }));
+  }
 
   render() {
     return <div>
@@ -47,5 +59,16 @@ export default class App extends Component {
           console.log(e);
         }
       });
+  }
+
+  getCurrentCognitoUser = () => {
+    let cognitoUser = userPool.getCurrentUser();
+
+    if (cognitoUser) {
+      cognitoUser.getSession((err, session) => {
+        let currentUserToken = session.getIdToken();
+        this.setState({ currentUserToken });
+      });
+    }
   }
 }
