60618bf5edb7cebf0f11cda77f96f13ae8d9f75c
pclundaahl <pc.lundaahl@gmail.com>
2018-11-22 13:04:54 -0800

    Default image added

--- src/client/components/profilePicture.js
+++ src/client/components/profilePicture.js
@@ -1,17 +1,16 @@
 import React, { Component } from "react";
 import './profilePicture.css';
 
+const DEFAULT_PICTURE_URL = 'https://s3-us-west-2.amazonaws.com/cloudception-bucket/public/images/user1.png';
+
 class ProfilePicture extends Component {
 
   render() {
     console.log("PictureProps", this.props)
-    return <img className="profile_picture img-fluid" src={this.props.picture} alt=""></img>;
+    return <img className="profile_picture img-fluid" src={
+      this.props.picture ? this.props.picture : DEFAULT_PICTURE_URL
+    } alt=""></img>;
   }
 }
-
-
-// ProfilePicture.defaultProps = {
-//   picture: 'http://pointe3.com/wp-content/uploads/2015/08/default-no-profile-pic.jpg'
-// }
 
 export default ProfilePicture;
