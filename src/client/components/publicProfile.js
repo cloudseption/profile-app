b78c31cd364873adec92a5b6bf38891bd2fc25ce
Alex <alexjcampbe@gmail.com>
2018-11-21 16:14:58 -0800

    Added working wizywig
    had to adjust name.js to be able to take html tags, conditionals are used in this and it make the profile profile page code look very ugly if someone could help me with getting the button to be in an appropriate place that be beautiful

--- src/client/components/publicProfile.js
+++ src/client/components/publicProfile.js
@@ -1,10 +1,11 @@
+import InlineEditor from '@ckeditor/ckeditor5-build-inline';
+import CKEditor from '@ckeditor/ckeditor5-react';
+import axios from "axios";
 import React, { Component } from "react";
+import BadgeFrame from "./badgeFrame";
+import Biography from "./biography";
 import Name from "./name";
 import ProfilePicture from "./profilePicture";
-import BadgeFrame from "./badgeFrame";
-import Badge from "./badge";
-import Biography from "./biography";
-import axios from "axios";
 
 class PublicProfile extends Component {
   // Hardcoded badgedata for now
@@ -29,22 +30,25 @@ class PublicProfile extends Component {
         text: `iggie wigge whatever`
       }
     ],
-    profile: {}
+    profile: {},
+    userId: null,
+    currUser: false
   };
 
   componentDidMount() {
-    const {handle} = this.props.match.params;
-
+    const { handle } = this.props.match.params;
+    this.setState({ userId: window.localStorage.userId });
     axios
       .get(
-      `http://localhost:8080/users/${handle}`
+        `http://localhost:8080/users/${handle}`
       )
       .then(res => {
         try {
           const profile = res.data;
-
           if (profile) {
-            this.setState({ profile });
+            let currUser = this.state.userId === profile.userId;
+            this.setState({ profile, currUser });
+            console.log(this.state.currUser);
           } else {
             this.setState({ profile: {} });
           }
@@ -52,32 +56,78 @@ class PublicProfile extends Component {
           console.log(e);
         }
       });
-    }
-  
+  }
+
+  saveUser() {
+    let data = [
+      { propName: "name", value: this.state.profile.name },
+      { propName: "description", value: this.state.profile.description },
+      { propName: "picture", value: this.state.profile.picture }
+    ];
+    axios
+      .patch(`http://localhost:8080/users/${this.state.userId}`,
+        data
+      );
+  }
 
   render() {
     return <React.Fragment>
-        <div className="card m-2">
-          <div className="card-header container banner_frame">
-            <div className="row">
-              <div className="col-12">
-                <Name value={this.state.profile.name} />
-              </div>
+      <div className="card m-2">
+        <div className="card-header container banner_frame">
+          <div className="row">
+            <div className="col-12">
+              {!this.state.currUser ? (
+                <Name value={this.state.profile.name} />) : (
+                  <CKEditor
+                    editor={InlineEditor}
+                    data={this.state.profile.name}
+                    onInit={editor => {
+                      // You can store the "editor" and use when it is needed.
+                      console.log('Editor is ready to use!', editor);
+                    }}
+                    onChange={(event, editor) => {
+                      const data = editor.getData();
+                      let prof = this.state.profile;
+                      prof.name = data;
+                      this.setState({ profile: prof })
+                    }}
+                  />)}
             </div>
-            <div className="row">
-              <div className="col-4 col-sm-5 col-md-7 col-lg-8 col-xl-9">
-                <ProfilePicture picture={this.state.profile.picture} />
-              </div>
-              <div className="col-8 col-sm-7 col-md-5 col-lg-4 col-xl-3 description_frame">
-                <BadgeFrame badgeData={this.state.badgeData} />
-              </div>
-            </div>
+
           </div>
-          <div className="card-body">
-          <Biography text={this.state.profile.description} />
+          <div className="row">
+            <div className="col-4 col-sm-5 col-md-7 col-lg-8 col-xl-9">
+              <ProfilePicture picture={this.state.profile.picture} />
+            </div>
+            <div className="col-8 col-sm-7 col-md-5 col-lg-4 col-xl-3 description_frame">
+              <BadgeFrame badgeData={this.state.badgeData} />
+            </div>
           </div>
         </div>
-      </React.Fragment>;
+        <div className="card-body">
+          {!this.state.currUser ? (
+            <Biography text={this.state.profile.description} />) : (
+              <CKEditor
+                editor={InlineEditor}
+                data={this.state.profile.description}
+                onInit={editor => {
+                  // You can store the "editor" and use when it is needed.
+                  console.log('Editor is ready to use!', editor);
+                }}
+                onChange={(event, editor) => {
+                  const data = editor.getData();
+                  let prof = this.state.profile;
+                  prof.description = data;
+                  this.setState({ profile: prof })
+                }}
+              />)}
+        </div>
+      </div>
+      {this.state.currUser &&
+        <div className="col-sm-9">
+          <button onClick={this.saveUser.bind(this)}>Save</button>
+        </div>}
+    </React.Fragment>;
   }
 }
 
