2462febe9c7da5d6571f862ca775e7e69fd6efc2
pclundaahl <pc.lundaahl@gmail.com>
2018-11-22 22:13:02 -0800

    Remove redundant launch-on-heroku.sh

--- launch-on-heroku.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Loading deployment package"
-wget https://s3-us-west-2.amazonaws.com/cloudception-bucket/public/deployment_package.zip
-unzip ./deployment_package.zip
-rm ./deployment_package.zip
