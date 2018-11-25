877039acfc5fbf4bd7702eb14804baa41b006a7e
Cameron Warje <cwarje@gmail.com>
2018-11-25 01:13:32 -0800

    Conditionally showing login/logout

--- src/client/components/Toolbar.js
+++ src/client/components/Toolbar.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import DrawerToggleButton from './DrawerToggleButton';
 import './Toolbar.css';
-import Home from './home';
+
+// if (window.localStorage.userId == "" || window.localStorage.userId == undefined) {
+//   window.location = "/auth/login.html";
+// }
 
 const toolbar = props => (
-  <header className='toolbar'>
-    <nav className='toolbar-navigation'>
-      <div className='toolbar-toggle-button'>
-          <DrawerToggleButton click={props.drawerClickHandler}/>
+  <header className="toolbar">
+    <nav className="toolbar-navigation">
+      <div className="toolbar-toggle-button">
+        <DrawerToggleButton click={props.drawerClickHandler} />
       </div>
-      <div className='toolbar-logo'>
+      <div className="toolbar-logo">
         <a href="/">BADGEBOOK</a>
       </div>
-      <div className="spacer"></div>
-      <div className='toolbar-navigation-items'>
+      <div className="spacer" />
+      <div className="toolbar-navigation-items">
         <ul>
           <li>
-            <a href={'/profile/' + window.localStorage.getItem('userId')}>Profile</a>
+            <a href={"/profile/" + window.localStorage.getItem("userId")}>
+              Profile
+            </a>
           </li>
           <li>
             <a href="/search">Search</a>
@@ -24,12 +29,12 @@ const toolbar = props => (
           <li>
             <a href="/home">Landing Page</a>
           </li>
-          <li>
-            <a href="/auth/login.html">Login</a>
-          </li>
-          <li>
-            <a onClick={props.signOut} href="#">Logout</a>
-          </li>
+          {
+            ( window.localStorage.userId == "" ||
+              window.localStorage.userId == undefined ) ?
+            <li><a href="/auth/login.html">Login</a></li> :
+            <li><a onClick={props.signOut} href="#">Logout</a></li>
+          }
         </ul>
       </div>
     </nav>
