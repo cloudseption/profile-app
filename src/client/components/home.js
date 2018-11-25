9f291a560fd9e8ab06a82ab7000a64b68d4c13ac
Cameron Warje <cwarje@gmail.com>
2018-11-25 00:22:27 -0800

    Major refactor

--- src/client/components/home.js
+++ src/client/components/home.js
@@ -12,81 +12,33 @@ class Home extends Component {
     componentDidMount = () => {
         axios.get(`${window.location.origin}/api/users/${window.localStorage.userId}/landing-data`)
         .then(response => {
-            console.log(response.data);
-
-            let app1 = {
-                appName: "Hangman",
-                imgUrl: "https://www.justinheidema.com/img/Hangman_Icon.png",
-                appUrl: "https://www.justinheidema.com",
-                text: "Not Added"
-            };
-
-            let app2 = {
-                appName: "InQuizitive",
-                imgUrl: "http://inquizitive-images.s3-website-us-west-2.amazonaws.com/quiz-badge-24x24.png",
-                appUrl: "http://inquizitive.s3-website-us-west-2.amazonaws.com/index.html",
-                text: "Not Added"
-            };
-
-            let app3 = {
-                appName: "Tiny Tank Game",
-                imgUrl: "https://png.icons8.com/app",
-                appUrl: "https://tiny-tanks.herokuapp.com/",
-                text: "Not Added"
-            };
-
-            if (response.data[0]) {
-                app1 = {
-                    appName: response.data[0].name,
-                    imgUrl: response.data[0]['img-url'],
-                    appUrl: response.data[0].link,
-                    text: response.data[0].data[0]
-                };
-            }
-
-            if (response.data[1]) {
-                app2 = {
-                    appName: response.data[1].name,
-                    imgUrl: response.data[1]['img-url'],
-                    appUrl: response.data[1].link,
-                    text: response.data[1].data[0]
-                };
-            }
-
-            if (response.data[2]) {
-                app3 = {
-                    appName: response.data[2].name,
-                    imgUrl: response.data[2]['img-url'],
-                    appUrl: response.data[2].link,
-                    text: response.data[2].data[0]
-                };
-            }
-
+            
             this.setState({
-                landingPageData: [
-                    app1,
-                    app2,
-                    app3,
-                ]  
+                landingPageData: response.data
             });
         });
     }
 
     render() { 
+        // How we're preventing non-logged in users from accessing landing pages. LOL
+        // TODO: Prevent in routing?
         if (window.localStorage.userId == "" || window.localStorage.userId == undefined) {
             window.location = "/auth/login.html";
         }
-        return (      
-            <div>
+
+        return <div>
             <List>
-            {this.state.landingPageData.map(appData => (
-                <AppCard key={appData.appName}
-                    appName={appData.appName} imgUrl={appData.imgUrl} 
-                    appUrl={appData.appUrl} text={appData.text}/>                
-            ))}
+              {this.state.landingPageData.map(appData => (
+                <AppCard
+                  key={appData.name}
+                  appName={appData.name}
+                  imgUrl={appData.imgUrl}
+                  appUrl={appData.appUrl}
+                  text={appData.data[0] ? appData.data[0] : "Not Added"}
+                />
+              ))}
             </List>
-            </div>
-        );
+          </div>;
     }
 }
  
