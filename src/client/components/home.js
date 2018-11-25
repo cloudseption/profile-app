e21010c352c94ea4cd4f391ef6cbbec7692c0738
Cameron Warje <cwarje@gmail.com>
2018-11-25 00:33:01 -0800

    Fixing indentation

--- src/client/components/home.js
+++ src/client/components/home.js
@@ -1,41 +1,50 @@
-import React, { Component } from 'react';
-import List from '@material-ui/core/List';
-import AppCard from './appCard.js';
-import axios from 'axios';
+import React, { Component } from "react";
+import List from "@material-ui/core/List";
+import AppCard from "./appCard.js";
+import axios from "axios";
 
 class Home extends Component {
-    
-    state = {  
-        landingPageData : []
-    }
+  state = {
+    landingPageData: []
+  };
 
-    componentDidMount = () => {
-        axios.get(`${window.location.origin}/api/users/${window.localStorage.userId}/landing-data`)
-        .then(response => {
-            this.setState({
-                landingPageData: response.data
-            });
+  componentDidMount = () => {
+    axios
+      .get(
+        `${window.location.origin}/api/users/${
+          window.localStorage.userId
+        }/landing-data`
+      )
+      .then(response => {
+        this.setState({
+          landingPageData: response.data
         });
-    }
+      });
+  };
 
-    render() { 
-        if (window.localStorage.userId == "" || window.localStorage.userId == undefined) {
-            window.location = "/auth/login.html";
-        }
-        return <div>
-            <List>
-              {this.state.landingPageData.map(appData => (
-                <AppCard
-                  key={appData.name}
-                  appName={appData.name}
-                  imgUrl={appData['img-url']}
-                  appUrl={appData.link}
-                  text={appData.data[0] ? appData.data[0] : "Not Added"}
-                />
-              ))}
-            </List>
-          </div>;
+  render() {
+    if (
+      window.localStorage.userId == "" ||
+      window.localStorage.userId == undefined
+    ) {
+      window.location = "/auth/login.html";
     }
+    return (
+      <div>
+        <List>
+          {this.state.landingPageData.map(appData => (
+            <AppCard
+              key={appData.name}
+              appName={appData.name}
+              imgUrl={appData["img-url"]}
+              appUrl={appData.link}
+              text={appData.data[0] ? appData.data[0] : "Not Added"}
+            />
+          ))}
+        </List>
+      </div>
+    );
+  }
 }
- 
+
 export default Home;
